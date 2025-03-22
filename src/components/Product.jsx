a998e500358aafe0ca1426b2ba82551b592e9e4b
rayxona <rayxona@gmail.com>
2025-03-22 22:11:06 +0500

    refactor Product component to open modal on cart button click

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -8,14 +8,13 @@ function Product({ d }) {
       {showModal && <Modal setShowModal={setShowModal} d={d} />}
       <div>
         <img
-          onClick={() => setShowModal(true)}
           className="product-image"
           src={d.image.thumbnail}
           alt={d.name}
           srcSet={`${d.image.desktop}`}
         />
         <div className="cart-button">
-          <button className="button">
+          <button onClick={() => setShowModal(true)} className="button">
             <img
               className="cart-image "
               src="./images/icon-add-to-cart.svg"
